592c18da172e5d1105046b14c78b67748bc50ca7
kellingraywilliamson <55054332+kellingraywilliamson@users.noreply.github.com>
2020-07-05 11:38:42 +0100

    Initial Commit

---
+++ 05_WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  05_WordScramble
+//
+//  Created by Adam Kellingray-Williamson on 05/07/2020.
+//  Copyright © 2020 Adam Kellingray-Williamson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
